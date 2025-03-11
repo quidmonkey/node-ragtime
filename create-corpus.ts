fa54bbe9ea4b0e55ec28f3987ae491ea7732f4a9
Abraham Walters <quidmonkey@gmail.com>
2025-03-10 20:51:39 -0400

    Lint and format create-corpus

--- create-corpus.ts
+++ create-corpus.ts
@@ -1,49 +1,49 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "node:fs";
+import path from "node:path";
 
-import { Command } from 'commander';
-import { sync as globSync } from 'glob';
- 
-import config from './environment';
+import { Command } from "commander";
+import { sync as globSync } from "glob";
+
+import config from "./environment";
 import {
-  Corpus,
-  convertDocument,
-  createDatabases,
-  getFilename,
-  saveDatabases
-} from './utils/etl';
+	type Corpus,
+	convertDocument,
+	createDatabases,
+	getFilename,
+	saveDatabases,
+} from "./utils/etl";
 
 const Program = new Command();
 
 const main = async (documentPath: string, databaseName: string) => {
-  const corpus: Corpus = {};
-  const filepaths = fs.lstatSync(documentPath).isDirectory()
-    ? globSync(`${documentPath}/*`)
-    : [documentPath];
+	const corpus: Corpus = {};
+	const filepaths = fs.lstatSync(documentPath).isDirectory()
+		? globSync(`${documentPath}/*`)
+		: [documentPath];
 
-  for (const filepath of filepaths) {
-    const text = await convertDocument(filepath);
-    const title = path.basename(filepath);
+	for (const filepath of filepaths) {
+		const text = await convertDocument(filepath);
+		const title = path.basename(filepath);
 
-    corpus[title] = text;
-  }
-  
-  const databases = await createDatabases(corpus);
+		corpus[title] = text;
+	}
 
-  const formattedModel = getFilename(config.LLM);
-  const formattedName = databaseName ?? getFilename(documentPath);
-  const keywordDatabasePath = `${formattedModel}-${formattedName}-keyword-database.json`;
-  const semanticDatabasePath = `${formattedModel}-${formattedName}-semantic-vector.db`;
+	const databases = await createDatabases(corpus);
 
-  await saveDatabases(databases, { keywordDatabasePath, semanticDatabasePath, });
+	const formattedModel = getFilename(config.LLM);
+	const formattedName = databaseName ?? getFilename(documentPath);
+	const keywordDatabasePath = `${formattedModel}-${formattedName}-keyword-database.json`;
+	const semanticDatabasePath = `${formattedModel}-${formattedName}-semantic-vector.db`;
 
-  process.exit();
+	await saveDatabases(databases, { keywordDatabasePath, semanticDatabasePath });
+
+	process.exit();
 };
 
-
-Program
-  .description('Generate an imvector and minisearch database for search queries using LLM Rag')
-  .argument('[documentPath]', 'Path to a document or directory of documents')
-  .argument('<databaseName>', 'Name of the database to save')
-  .action(main)
-  .parse();
+Program.description(
+	"Generate an imvector and minisearch database for search queries using LLM Rag",
+)
+	.argument("[documentPath]", "Path to a document or directory of documents")
+	.argument("<databaseName>", "Name of the database to save")
+	.action(main)
+	.parse();
