b37df749c5e0ec705749bc82b6b1d82e514778b5
Abraham Walters <quidmonkey@gmail.com>
2025-03-10 20:51:01 -0400

    Lint and format compare-models

--- compare-models.ts
+++ compare-models.ts
@@ -1,41 +1,40 @@
 /**
- * Rag-based LLM Nodejs script that compares the performance of different LLMs 
+ * Rag-based LLM Nodejs script that compares the performance of different LLMs
  * by using Ollama LLM to answer a query about Sherlock Holmes
  */
-import config from './environment';
+import config from "./environment";
 
-import { getFilename, loadSemanticDatabase } from './utils/etl';
-import { sherlockChat } from './utils/sherlock';
-
+import { getFilename, loadSemanticDatabase } from "./utils/etl";
+import { sherlockChat } from "./utils/sherlock";
 
 const main = async () => {
-  const queries = [
-    "What is Sherlock's favorite food?",
-    "What does Sherlock like to wear?",
-    "How many Sherlock Holmes stories are there?",
-    "Who wrote Sherlock Holmes?",
-    "Who is Sherlock's arch-enemy?",
-  ];
+	const queries = [
+		"What is Sherlock's favorite food?",
+		"What does Sherlock like to wear?",
+		"How many Sherlock Holmes stories are there?",
+		"Who wrote Sherlock Holmes?",
+		"Who is Sherlock's arch-enemy?",
+	];
 
-  for (const model of config.COMPARE_MODELS) {
-    const databasePath = `${getFilename(model)}-semantic-vector.db`;
-    const db = await loadSemanticDatabase(databasePath);
+	for (const model of config.COMPARE_MODELS) {
+		const databasePath = `${getFilename(model)}-semantic-vector.db`;
+		const db = await loadSemanticDatabase(databasePath);
 
-    console.log('');
-    console.log('### Comparing Model', model, '###');
+		console.log("");
+		console.log("### Comparing Model", model, "###");
 
-    for (const query of queries) {
-      console.log('');
-      console.log('🤔', query);
-      console.log('');
+		for (const query of queries) {
+			console.log("");
+			console.log("🤔", query);
+			console.log("");
 
-      const res = await sherlockChat(db, query);
+			const res = await sherlockChat(db, query);
 
-      console.log('🤖', res);
-    }
-  }
+			console.log("🤖", res);
+		}
+	}
 
-  process.exit();
+	process.exit();
 };
 
 main();
